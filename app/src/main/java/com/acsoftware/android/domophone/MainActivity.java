25f90dc21a0be7db1f6e1866d070dfd6c3a8ca8b
Przemyslaw Zygmunt <p.zygmunt@acsoftware.pl>
2018-04-02 14:40:58 +0200

    Start from clean Android Studio project skeleton again

---
+++ app/src/main/java/com/acsoftware/android/domophone/MainActivity.java
@@ -0,0 +1,13 @@
+package com.acsoftware.android.domophone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
